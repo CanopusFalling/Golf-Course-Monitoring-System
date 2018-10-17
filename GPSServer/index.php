d186b0fc4311d1d5a8d3767c276ad4e8e3c53207
SamTheDude <kent_sam@outlook.com>
2018-10-17 12:09:48 +0100

    17/10/2018

--- GPSServer/index.php
+++ GPSServer/index.php
@@ -1,3 +1,12 @@
 <?php
 echo $_GET["ID"] . " : " . $_GET["Lat"] . " : " . $_GET["Long"];
+
+$date = date('m-d-Y h:i:s', time());
+$Command = "INSERT INTO GPSData (UserID, DateTimeStamp, Longitude, Latitude) VALUES (" . $_GET["ID"] . ", '" . $date . "', " . $_GET["Lat"] . ", " . $_GET["Long"] . ");";
+
+echo "\n" . $Command;
+
+$PDO = new PDO('sqlite:/home/samkent/Documents/GolfCourseGPSManagementSystem/Database/GolfData.db');
+$PDO->query($Command);
+
 ?>
