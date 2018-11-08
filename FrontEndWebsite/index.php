b1106444cf77dc4fc7f2c7562dde3aac15775614
SamTheDude <kent_sam@outlook.com>
2018-11-08 13:04:56 +0000

    Added pointers code, needs ajax finesse.

--- FrontEndWebsite/index.php
+++ FrontEndWebsite/index.php
@@ -7,9 +7,7 @@
 <link rel="stylesheet" href="Styles.css">
 </Head>
 
-<div class="image-main">
-    <img src="ImageGallery/CourseMap.png" alt="Course Map">
-    <div class="overlay" src="Point.png"></div>
-</div>
+<div class="Course-Image"><img src="ImageGallery/CourseMap.png" alt="Course Map"></div>
+<div class="Point-Overlay"><img src="ImageGallery/Point.png" alt="Course Map"></div>
 
 
